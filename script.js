0e10cb7b2c52feab284fe1a33e3c44b4394409e9
Akash Badadani <103527635+akashb2003@users.noreply.github.com>
2024-09-16 20:03:08 +0530

    bubble, quick and merge sort algorithms

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let array = [];
 let arraySize = 50;
 let animationSpeed = 50; // To Control animation speed
-
+ 
 // Generate a random array
 function generateArray() {
   arraySize = document.getElementById("sizeRange").value;
